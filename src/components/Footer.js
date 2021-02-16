5128233479241744e9cc5903354ae16dbdabc253
DeeptiEdcast <52028816+DeeptiEdcast@users.noreply.github.com>
2021-02-16 20:27:33 +0530

    Footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,13 +1,26 @@
-import React from 'react'
+import React from "react";
+import { LABELS_PLAYER } from "../util/StringConstant";
 
 function Footer() {
     return (
-        <div id="footer" className="footer-panel">
-            <div  className="footer-panel-copyright-text">
-                <p>Footer</p>
+        <div id='footer' className='footer-panel'>
+            <div className='footer-panel-copyright-text'>
+                &copy; MyGuide by Edcast
+                <span className='footer-text-separator'>
+                    {LABELS_PLAYER.en.separator}
+                </span>
+                <a id='gss-footer-privacy-policy' target='_blank'>
+                    {LABELS_PLAYER.en.privacyPolicy} {LABELS_PLAYER.cookie}
+                </a>
+                <span className='footer-text-separator'>
+                    {LABELS_PLAYER.en.separator}
+                </span>
+                <a id='gss-footer-terms-of-service' target='_blank'>
+                    {LABELS_PLAYER.en.termsOfService}
+                </a>
             </div>
         </div>
-    )
+    );
 }
 
 export default Footer;
